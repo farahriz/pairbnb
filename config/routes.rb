a2631e7ebb7e9fe5ee13040868324a7a607f2229
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-09 18:03:50 +0800

    Set up registration and log in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'register', to: 'sessions#register'
+  get 'login', to: 'sessions#login'
+
+  # get 'sessions/register'
+  # get 'sessions/login'
+
+  get 'users/index'
+  get 'users/show'
+  
 end
