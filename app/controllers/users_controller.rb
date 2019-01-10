d1cfae4056232e35a3346ac556dfb7fda2af5a92
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-10 21:54:56 +0800

    Updated user model to validate prescence of column info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,35 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  # def update
+  #   user = current_account.people.find(params[:id])
+  #   user.update!(user_params)
+  #   redirect_to user
+  # end
+
+
+  def new
+  	# byebug
+    @user = User.new(user_params)
+    render template: "users/new"
+  end
+
+  def create
+  	# byebug
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :handle, :first_name, :last_name, :password)
+    end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  root 'welcome#index'
+root 'welcome#index'
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, controller: "users", only: [:create, :edit, :update] do
+  resources :users, only: [:create, :edit, :update] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
@@ -12,14 +12,7 @@ Rails.application.routes.draw do
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
-  
-
   get "/sign_up" => "users#new", as: "sign_up"
-
-
-  get "/users" => "users#index", as: "users_index"
-  get "/users/show" => "users#show", as: "user_profile"
-  get 'users/edit'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
