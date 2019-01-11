9a338a6d755a4dfaa1c247e0a9792818b721da72
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-11 14:25:18 +0800

    Created private method to allow parameters in form to pass to listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,4 +4,10 @@ class ListingsController < ApplicationController
 
   def create
   end
+
+  private
+  def post_params
+    params.require(:listing).permit(:name, :desctiption, :location, :policy, :num_bedroom, :num_bed, :num_bathroom, :max_guests, :all_tags)
+  end
+
 end
