4f2e4d55eb991eb173fd45c81d1a5ac425600156
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-11 14:00:08 +0800

    Created model for tagging join table

---
+++ db/migrate/20190111055550_add_foreign_key_listing_to_tags.rb
@@ -0,0 +1,4 @@
+class AddForeignKeyListingToTags < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_11_050700) do
+ActiveRecord::Schema.define(version: 2019_01_11_054943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,25 @@ ActiveRecord::Schema.define(version: 2019_01_11_050700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "location"
+    t.string "policy"
+    t.integer "num_bedroom"
+    t.integer "num_bed"
+    t.integer "num_bathroom"
+    t.integer "max_guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
