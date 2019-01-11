283008553fc70176cfec9cf215efe7cbee4c802a
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-11 13:55:16 +0800

    Defined table structure for listings and tags

--- db/migrate/20190111053245_create_listings.rb
+++ db/migrate/20190111053245_create_listings.rb
@@ -1,7 +1,14 @@
 class CreateListings < ActiveRecord::Migration[5.2]
   def change
     create_table :listings do |t|
-
+      t.string :name
+      t.string :description
+      t.string :location
+      t.string :policy
+      t.integer :num_bedroom	
+      t.integer :num_bed	
+      t.integer :num_bathroom	
+      t.integer :max_guests	
       t.timestamps
     end
   end
