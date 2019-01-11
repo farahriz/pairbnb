0e0cc24ee28f2ce6909609426a3c3b2135c9a69c
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-11 14:06:41 +0800

    Ran migration to create tagging join table and index tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_11_054943) do
+ActiveRecord::Schema.define(version: 2019_01_11_060310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,20 @@ ActiveRecord::Schema.define(version: 2019_01_11_054943) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "listing_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_taggings_on_listing_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +69,6 @@ ActiveRecord::Schema.define(version: 2019_01_11_054943) do
   end
 
   add_foreign_key "authentications", "users"
+  add_foreign_key "taggings", "listings"
+  add_foreign_key "taggings", "tags"
 end
