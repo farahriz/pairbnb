d5bbaefd2e8700e84ee629bbd84074120c0d7314
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-14 16:07:05 +0800

    Added avatars to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_14_064244) do
+ActiveRecord::Schema.define(version: 2019_01_14_080607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_01_14_064244) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "price"
+    t.json "avatars"
   end
 
   create_table "taggings", force: :cascade do |t|
