717d1154e4c2883970c040838097caf493bc0239
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-15 14:30:44 +0800

    Allow params from reservations to pass into form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,6 +6,7 @@ class ReservationsController < ApplicationController
 
 	def create
 		@reservation = current_user.reservations.new(reservation_params)
+		@reservation.listing = @listing
 
 		if @reservation.save
 			redirect_to reservation_path(@listing), notice: "Your reservation has been successfully created"
@@ -15,5 +16,10 @@ class ReservationsController < ApplicationController
 		end
 
 	end	
+
+	private
+		def reservation_params
+    		params.require(:reservation).permit(:check_in, :check_out)
+		end
 
 end
